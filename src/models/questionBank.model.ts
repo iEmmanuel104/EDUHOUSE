0993f97e4d61da9abbc2be3d9dcbc8c045c402be
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-09-21 16:36:06 +0100

    feat: Add QuestionBank model for managing question bank data

---
+++ src/models/questionBank.model.ts
@@ -0,0 +1,56 @@
+import {
+    Table, Column, Model, DataType, IsUUID, PrimaryKey, Default, Validate, BelongsToMany,
+} from 'sequelize-typescript';
+import Assessment, { IAssessment } from './assessment/evaluation.model';
+import AssessmentQuestion from './assessment/questions.model';
+
+@Table
+export default class QuestionBank extends Model<QuestionBank | IQuestionBank> {
+    @IsUUID(4)
+    @PrimaryKey
+    @Default(DataType.UUIDV4)
+    @Column
+    id: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    question: string;
+
+    @Validate({
+        isValidLength(value: { option: string; text: string }[]) {
+            if (value.length < 2 || value.length > 4) {
+                throw new Error('Options must contain between 2 and 4 entries.');
+            }
+        },
+    })
+    @Column(DataType.JSONB)
+    options: { option: string; text: string }[];
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    answer: string;
+
+    @Column({
+        type: DataType.ARRAY(DataType.STRING),
+        allowNull: false,
+    })
+    categories: string[];
+
+    @BelongsToMany(() => Assessment, () => AssessmentQuestion)
+    assessments: Assessment[];
+}
+
+
+export interface IQuestionBank {
+    id?: string;
+    question: string;
+    options: { option: string; text: string }[];
+    answer: string;
+    categories: string[];
+    assessments?: IAssessment[];
+}
+
