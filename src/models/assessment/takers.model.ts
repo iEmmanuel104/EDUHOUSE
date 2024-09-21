33b22424ddcef94ffc7cae63eaacdcb37dcfc64a
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-09-21 16:37:10 +0100

    feat: Add AssessmentTaker model for managing assessment taker data

---
+++ src/models/assessment/takers.model.ts
@@ -0,0 +1,64 @@
+/* eslint-disable no-unused-vars */
+import {
+    Table, Column, Model, DataType, ForeignKey, Default, IsUUID, PrimaryKey,
+} from 'sequelize-typescript';
+import Assessment from './evaluation.model';
+import Teacher from '../teacher.model';
+
+export enum AssessmentTakerStatus {
+    PENDING = 'pending',
+    ONGOING = 'ongoing',
+    COMPLETED = 'completed',
+}
+
+@Table
+export default class AssessmentTaker extends Model<AssessmentTaker | IAssessmentTaker> {
+
+    @IsUUID(4)
+    @PrimaryKey
+    @Default(DataType.UUIDV4)
+    @Column
+        id: string;
+
+    @ForeignKey(() => Assessment)
+    @Column
+        assessmentId: string;
+
+    @ForeignKey(() => Teacher)
+    @Column
+        teacherId: string;
+
+    @Column({
+        type: DataType.ENUM,
+        values: Object.values(AssessmentTakerStatus),
+        defaultValue: AssessmentTakerStatus.PENDING,
+    })
+        status: AssessmentTakerStatus;
+
+    @Column(DataType.DATE)
+        dueDate: Date;
+
+    @Column(DataType.DATE)
+        startedAt: Date;
+
+    @Column(DataType.DATE)
+        completedAt: Date;
+
+    @Column(DataType.JSONB)
+        answers: { questionId: string; answer: string }[];
+
+    @Column(DataType.FLOAT)
+        score: number;
+}
+
+export interface IAssessmentTaker {
+    id?: string;
+    assessmentId: string;
+    teacherId: string;
+    status: AssessmentTakerStatus;
+    dueDate: Date;
+    startedAt: Date;
+    completedAt: Date;
+    answers: { questionId: string; answer: string }[];
+    score: number;
+}
