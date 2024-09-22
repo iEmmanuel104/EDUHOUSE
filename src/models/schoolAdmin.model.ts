95720b15ff9440c3583ab2dc0b333b5b19f34e95
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-09-23 00:42:47 +0100

    Refactor SchoolAdmin model to include permissions for school administrators

--- src/models/schoolAdmin.model.ts
+++ src/models/schoolAdmin.model.ts
@@ -11,6 +11,21 @@ export enum AdminRole {
     GUEST = 'guest',
 }
 
+export enum SchoolAdminPermissions {
+    CREATE_SCHOOL = 'create_school',
+    UPDATE_SCHOOL = 'update_school',
+    DELETE_SCHOOL = 'delete_school',
+    DISABLE_SCHOOL = 'disable_school',
+    CREATE_TEACHER = 'create_teacher',
+    UPDATE_TEACHER = 'update_teacher',
+    DELETE_TEACHER = 'delete_teacher',
+    CREATE_ASSESSMENT = 'create_assessment',
+    UPDATE_ASSESSMENT = 'update_assessment',
+    DELETE_ASSESSMENT = 'delete_assessment',
+    ADD_ASSESSMENT_TAKER = 'create_assessment_taker',
+    REMOVE_ASSESSMENT_TAKER = 'update_assessment_taker',
+}
+
 @Scopes(() => ({
     School: (schoolId: number) => ({
         where: { schoolId },
@@ -35,6 +50,20 @@ export default class SchoolAdmin extends Model<SchoolAdmin | ISchoolAdmin> {
     })
         role: AdminRole;
 
+    @Column({
+        type: DataType.ARRAY(DataType.ENUM(...Object.values(SchoolAdminPermissions))),
+        allowNull: true,
+        set(value: SchoolAdminPermissions[]) {
+            if (Array.isArray(value)) {
+                const uniqueRestrictions = [...new Set(value)];
+                this.setDataValue('restrictions', uniqueRestrictions);
+            } else {
+                this.setDataValue('restrictions', value);
+            }
+        },
+    })
+        restrictions: SchoolAdminPermissions[];
+    
     @BelongsTo(() => Admin)
         admin: Admin;
 
@@ -46,4 +75,5 @@ export interface ISchoolAdmin {
     adminId: string;
     schoolId: number;
     role: AdminRole;
+    restrictions?: SchoolAdminPermissions[];
 }
