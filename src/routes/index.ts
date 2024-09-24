3f1ddd0be801787de70d136418e38aacaeb194ae
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-09-24 20:52:17 +0100

    Refactor routes/index.ts to add school route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,13 +2,14 @@ import { Router } from 'express';
 import authRoute from './auth.routes';
 import userRoute from './user.routes';
 import AdminRoute from './Admin/admin.routes';
-
+import SchoolRoute from './school.routes';
 
 const router = Router();
 
 router
     .use('/auth', authRoute)
     .use('/iamEduh', AdminRoute)
+    .use('/school', SchoolRoute)
     .use('/user', userRoute);
 
 export default router;
