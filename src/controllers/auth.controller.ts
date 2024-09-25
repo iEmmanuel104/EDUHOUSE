6e7728dcd692615dff28e040542746ab72b19475
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-09-25 21:26:49 +0100

    Refactor AuthController to add signup, email verification, password reset, and password change functionality

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -9,22 +9,26 @@ import UserService, { IDynamicQueryOptions } from '../services/user.service';
 import { AdminAuthenticatedRequest, AuthenticatedRequest } from '../middlewares/authMiddleware';
 import { Transaction } from 'sequelize';
 import { ISchoolTeacher } from '../models/schoolTeacher.model';
+import Validator from '../utils/validators';
+import { WEBSITE_URL } from '../utils/constants';
 
 export default class AuthController {
 
     static async signup(req: Request, res: Response) {
-        const { email, firstName, lastName, schoolId, displayImage, gender, phone, isTeachingStaff, isActive, classAssigned } = req.body;
+        const { email, firstName, lastName, otherName, dob, schoolId, displayImage, gender, phone, isTeachingStaff, isActive, classAssigned } = req.body;
 
         const userData = {
             email,
             firstName,
             lastName,
+            otherName,
             status: {
                 activated: false,
                 emailVerified: false,
             },
             displayImage,
             gender,
+            dob,
             phone: {
                 countryCode: phone?.countryCode || '+234',
                 number: phone?.number,
@@ -102,7 +106,7 @@ export default class AuthController {
 
         res.status(201).json({
             status: 'success',
-            message: 'Email verification code sent successfully',
+            message: 'Registration Successful, Email verification code sent to your email',
             data: {
                 teacher: user,
             },
@@ -110,7 +114,7 @@ export default class AuthController {
     }
 
     static async verifyEmail(req: Request, res: Response) {
-        const { otpCode, email } = req.body;
+        const { otpCode, email, password } = req.body;
 
         await Database.transaction(async (transaction: Transaction) => {
 
@@ -125,25 +129,20 @@ export default class AuthController {
                 status: { ...user.status, emailVerified: true, activated: true },
             }, { transaction });
 
-            await AuthUtil.deleteToken({ user, tokenType: 'emailverification', tokenClass: 'code' });
-
-            const updatedUser = await UserService.viewSingleUser(user.id);
-
-            // password is the user's lastname in lowercase
-            const password = user.lastName.toLowerCase();
-
+            const validPassword = Validator.isValidPassword(password);
+            if (!validPassword) {
+                throw new BadRequestError('Invalid password');
+            }
+            
             // Create a new password for the user
             await Password.create({ userId: user.id, password: password }, { transaction });
-
-            const accessToken = await AuthUtil.generateToken({ type: 'access', user });
+                        
+            await AuthUtil.deleteToken({ user, tokenType: 'emailverification', tokenClass: 'code' });
 
             res.status(200).json({
                 status: 'success',
-                message: 'Email verified successfully',
-                data: {
-                    teacher: updatedUser,
-                    accessToken,
-                },
+                message: 'Email verified successfully, proceed to login',
+                data: null,
             });
         });
     }
@@ -180,6 +179,119 @@ export default class AuthController {
         res.status(200).json({
             status: 'success',
             message: 'Email verification code resent successfully',
+            data: null,
+        });
+    }
+
+    static async forgotPassword(req: Request, res: Response) {
+        const { email, redirectUrl } = req.body;
+
+        console.log({ email, redirectUrl });
+
+        const user = await UserService.viewSingleUserByEmail(email);
+
+        if (!user) {
+            throw new BadRequestError('Oops User not found');
+        }
+
+        const resetToken = await AuthUtil.generateCode({ type: 'passwordreset', identifier: user.id, expiry: 60 * 10 });
+        const redirectLink: string = redirectUrl ? redirectUrl : `${WEBSITE_URL}/reset-password`;
+
+        const resetLink = `${redirectLink}?prst=${resetToken}&e=${encodeURIComponent(email)}`;
+
+        const templateData = {
+            link: resetLink,
+            name: user.firstName,
+        };
+
+        // TODO: Send email with the reset password link with the resetToken as query param
+        await emailService.send({
+            email: 'batch',
+            subject: 'Password Reset 🔑',
+            from: 'auth',
+            isPostmarkTemplate: true,
+            postMarkTemplateAlias: 'password-reset',
+            postmarkInfo: [{
+                postMarkTemplateData: templateData,
+                receipientEmail: email,
+            }],
+            html: await new EmailTemplate().forgotPassword({ link: resetLink, name: user.firstName }),
+        });
+
+        res.status(200).json({
+            status: 'success',
+            message: 'Reset password instructions sent successfully',
+            data: null,
+        });
+    }
+
+    static async resetPassword(req: Request, res: Response) {
+        const { resetToken, email, newPassword }: { resetToken: string, email: string, newPassword: string } = req.body;
+
+        const validPassword = Validator.isValidPassword(newPassword);
+        if (!validPassword) {
+            throw new BadRequestError('Invalid password format');
+        }
+
+        const user = await UserService.viewSingleUserByEmail(email);
+
+        const validCode = await AuthUtil.compareCode({ user, tokenType: 'passwordreset', token: resetToken });
+
+        if (!validCode) {
+            throw new BadRequestError('Invalid reset token');
+        }
+
+        const password = await user.$get('password');
+        if (!password) {
+            // if email is verified and not activated, create new password for user
+            if (!user.status.activated) {
+                if (!user.status.emailVerified) {
+                    user.update({ status: { ...user.status, emailVerified: true } });
+                }
+                // create new password for user
+                await Password.create({ userId: user.id, password: newPassword });
+            } else {
+                throw new ForbiddenError('Please contact support');
+            }
+        } else {
+            // await Password.update({ password: newPassword }, { where: { id: password.id } });
+            password.password = newPassword;
+            await password.save();
+        }
+
+        // await AuthUtil.deleteToken({ user, tokenType: 'passwordreset', tokenClass: 'token' });
+
+        res.status(200).json({
+            status: 'success',
+            message: 'Password reset successfully. Please login with your new password',
+            data: null,
+        });
+    }
+
+    static async changePassword(req: AuthenticatedRequest, res: Response) {
+        const { oldPassword, newPassword }: { oldPassword: string, newPassword: string } = req.body;
+
+        const validPassword = Validator.isValidPassword(newPassword);
+        if (!validPassword) {
+            throw new BadRequestError('Invalid password');
+        }
+
+        const { id } = req.user;
+        const user = await UserService.viewSingleUser(id);
+
+        const password = await user.$get('password');
+        if (!password) throw new ForbiddenError('Please contact support');
+
+        const validOldPassword = await password.isValidPassword(oldPassword);
+        if (!validOldPassword) {
+            throw new BadRequestError('Invalid old password');
+        }
+
+        await password.update({ password: newPassword });
+
+        res.status(200).json({
+            status: 'success',
+            message: 'Password changed successfully',
             data: null,
         });
     }
