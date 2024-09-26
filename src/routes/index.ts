d5ece244443f7fa61e00f1d467f268f317ba36f8
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-09-26 14:02:54 +0100

    Refactor routes to include assessment module

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import userRoute from './user.routes';
 import AdminRoute from './Admin/admin.routes';
 import SchoolRoute from './school.routes';
+import AssessmentRoute from './assessment.routes';
 
 const router = Router();
 
 router
     .use('/iamEduh', AdminRoute)
     .use('/school', SchoolRoute)
+    .use('/assessment', AssessmentRoute)
     .use('/teacher', userRoute);
 
 export default router;
