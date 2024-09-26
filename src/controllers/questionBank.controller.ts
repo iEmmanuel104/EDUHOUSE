901d4da84011034dc7771d01f743a63d0c1c35ca
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-09-26 14:47:40 +0100

    Refactor questionBank.controller.ts to include assessment questions and permissions

--- src/controllers/questionBank.controller.ts
+++ src/controllers/questionBank.controller.ts
@@ -1,76 +1,68 @@
 import { Request, Response } from 'express';
 import QuestionBankService, { IViewQuestionsQuery } from '../services/questionBank.service';
-import { AuthenticatedRequest } from '../middlewares/authMiddleware';
+import { AdminAuthenticatedRequest } from '../middlewares/authMiddleware';
+import { SchoolAdminPermissions } from '../models/schoolAdmin.model';
+import { Database } from '../models';
+import { Transaction } from 'sequelize';
 
 export default class QuestionBankController {
-    static async createQuestion(req: AuthenticatedRequest, res: Response) {
+    static async addOrUpdateAssessmentQuestion(req: AdminAuthenticatedRequest, res: Response) {
+        const { assessmentId } = req.params;
         const questionData = req.body;
 
-        const newQuestion = await QuestionBankService.addQuestion(questionData);
+        await Database.transaction(async (transaction: Transaction) => {
 
-        res.status(201).json({
-            status: 'success',
-            message: 'Question created successfully',
-            data: {
-                question: newQuestion,
-            },
+            const { question, created } = await QuestionBankService.addOrUpdateAssessmentQuestion(
+                assessmentId,
+                questionData,
+                req.admin,
+                SchoolAdminPermissions.UPDATE_ASSESSMENT,
+                transaction
+            );
+
+            res.status(created ? 201 : 200).json({
+                status: 'success',
+                message: created ? 'Question added to assessment successfully' : 'Assessment question updated successfully',
+                data: {
+                    question,
+                },
+            });
         });
     }
 
-    static async getQuestions(req: Request, res: Response) {
+    static async removeQuestionFromAssessment(req: AdminAuthenticatedRequest, res: Response) {
+        const { assessmentId, questionId } = req.params;
+        await Database.transaction(async (transaction: Transaction) => {
+            await QuestionBankService.removeQuestionFromAssessment(
+                assessmentId,
+                questionId,
+                req.admin,
+                SchoolAdminPermissions.UPDATE_ASSESSMENT,
+                transaction
+            );
+
+            res.status(200).json({
+                status: 'success',
+                message: 'Question removed from assessment successfully',
+                data: null,
+            });
+        });
+    }
+
+    static async getAssessmentQuestions(req: Request, res: Response) {
+        const { assessmentId } = req.params;
         const queryData: IViewQuestionsQuery = req.query;
 
-        const { questions, count, totalPages } = await QuestionBankService.viewQuestions(queryData);
+        const { questions, count, totalPages } = await QuestionBankService.viewAssessmentQuestions(assessmentId, queryData);
 
         res.status(200).json({
             status: 'success',
-            message: 'Questions retrieved successfully',
+            message: 'Assessment questions retrieved successfully',
             data: {
                 questions,
                 count,
                 totalPages,
             },
-        });
-    }
-
-    static async getQuestion(req: Request, res: Response) {
-        const { id } = req.params;
-
-        const question = await QuestionBankService.viewSingleQuestion(id);
-
-        res.status(200).json({
-            status: 'success',
-            message: 'Question retrieved successfully',
-            data: {
-                question,
-            },
-        });
-    }
-
-    static async updateQuestion(req: AuthenticatedRequest, res: Response) {
-        const { id } = req.params;
-        const updateData = req.body;
-
-        const updatedQuestion = await QuestionBankService.updateQuestion(id, updateData);
-
-        res.status(200).json({
-            status: 'success',
-            message: 'Question updated successfully',
-            data: {
-                question: updatedQuestion,
-            },
-        });
-    }
-
-    static async deleteQuestion(req: AuthenticatedRequest, res: Response) {
-        const { id } = req.params;
-
-        await QuestionBankService.deleteQuestion(id);
-
-        res.status(200).json({
-            status: 'success',
-            message: 'Question deleted successfully',
-            data: null,
         });
     }
 }
